7d25f9a5e031438a550edbd6d8c368657f5638a7
minhee <76712706+himinsunsine@users.noreply.github.com>
2023-07-21 16:18:24 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,6 @@
+S = input()
+
+bet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+for i in range(len(bet)):
+    print(S.find(bet[i]),end=' ')
